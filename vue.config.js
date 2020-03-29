2bdfd5c695b9999241b212ca2e462bad46e38143
Alexander Bell-Towne <alexander@bell-towne.com>
2020-03-29 00:11:40 -0400

    public path

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const PrerenderSpaPlugin = require('prerender-spa-plugin');
 
 module.exports = {
   configureWebpack: {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/www.bell-towne.com/'
+      : '/',
     plugins: [
       new PrerenderSpaPlugin(
         // Absolute path to compiled SPA
