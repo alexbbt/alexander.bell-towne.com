0b518678ad70680788d1e62eacc545f8a612e70f
Alexander Bell-Towne <alexander@bell-towne.com>
2020-04-02 00:32:10 -0400

    multiple files in ls

--- src/modules/shell/path/List.ts
+++ src/modules/shell/path/List.ts
@@ -30,30 +30,51 @@ class List implements Command {
     return fileBasedTabComplete(args);
   }
 
-  run(args: string[]): CommandOutput {
-    const parsedFile = parseFileName(args[0]);
-    switch (parsedFile) {
-      case '':
-      case '.':
-      case './':
-      case null:
-      case undefined:
-        return {
-          status: 0,
-          output: FILES.map((f) => `  ${f}`).join('\n'),
-        };
-      default:
-        if (FILES.includes(parsedFile)) {
-          return {
-            status: 0,
-            output: parsedFile,
-          };
-        }
-        return {
-          status: 1,
-          output: `ls: ${args[0]}: No such file or directory`,
-        };
+  run(files: string[]): CommandOutput {
+    const filteredFiles = files.filter((f) => f);
+    if (filteredFiles.length === 0) {
+      return {
+        status: 1,
+        output: 'cat: must have an argument',
+      };
     }
+
+    const output: CommandOutput = {
+      status: 0,
+      output: '',
+    };
+
+    let errorOutput = '';
+
+    filteredFiles.forEach((file) => {
+      const parsedFile = parseFileName(file);
+
+      switch (parsedFile) {
+        case '':
+        case '.':
+        case './':
+        case null:
+        case undefined:
+          if (filteredFiles.length > 1) {
+            output.output += `\n${file}:\n`;
+          }
+          output.output += FILES.map((f) => `  ${f}`).join('\n');
+          break;
+        default:
+          if (FILES.includes(parsedFile)) {
+            output.output += parsedFile;
+          } else {
+            errorOutput += `ls: ${file}: No such file or directory\n`;
+            output.status = 1;
+          }
+      }
+
+      output.output += '\n';
+    });
+
+    output.output = errorOutput + output.output;
+
+    return output;
   }
 }
 
