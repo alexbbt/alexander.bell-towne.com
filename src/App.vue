a588ec4188675e7b040338f4cd730b8de0b0b9e7
Alexander Bell-Towne <alexbbt@squareup.com>
2020-07-09 12:48:56 -0400

    new meta tags

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,55 @@
 <script>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import myFace from './assets/my-face.jpg';
 
 import 'flexboxgrid';
 import 'terminal.css';
+
+const TITLE = 'Alexander Bell-Towne';
+const DESCRIPTION = 'I work as a Senior Software Engineer at Square, Inc.';
+const URL = 'https://bell-towne.com';
+const IMAGE = myFace;
 
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
+  },
+  head: {
+    title: TITLE,
+    meta: [
+      { name: 'description', content: DESCRIPTION },
+
+      // OpenGraph data (Most widely used)
+      { property: 'og:title', content: TITLE },
+      { property: 'og:site_name', content: 'bell-towne.com' },
+      // The list of types is available here: http://ogp.me/#types
+      { property: 'og:type', content: 'website' },
+      // Should the the same as your canonical link, see below.
+      { property: 'og:url', content: URL },
+      { property: 'og:image', content: IMAGE },
+      // Often the same as your meta description, but not always.
+      { property: 'og:description', content: DESCRIPTION },
+
+      // Twitter card
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: URL },
+      { name: 'twitter:title', content: TITLE },
+      { name: 'twitter:description', content: DESCRIPTION },
+      // Your twitter handle, if you have one.
+      { name: 'twitter:creator', content: '@alexbbt' },
+      { name: 'twitter:image:src', content: IMAGE },
+
+      // Google / Schema.org markup:
+      { itemprop: 'name', content: TITLE },
+      { itemprop: 'description', content: DESCRIPTION },
+      { itemprop: 'image', content: IMAGE },
+    ],
+    link: [
+      { rel: 'canonical', href: URL },
+    ],
   },
 };
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import VueMeta from 'vue-meta';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(VueMeta, {
+  keyName: 'head',
+  // optional pluginOptions
+  refreshOnceOnNavigation: true,
+});
 
 Vue.config.productionTip = false;
 
